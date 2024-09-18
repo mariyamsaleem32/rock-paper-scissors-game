ab85e594bb7a5b0042a21d19892b34b013a2636e
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-09-18 16:07:38 +0500

    update code

--- script.js
+++ script.js
@@ -12,18 +12,23 @@ const choice = document.querySelectorAll(".choice");
 const getCompChoice = ( () => {
     let chicesOfComp = ["rock","paper","scissors "];
     let indx = Math.floor(Math.random()*3);
-    console.log(chicesOfComp[indx]);
+     return chicesOfComp[indx]
     });
 
 choice.forEach( (myChoice) => {
-    myChoice.addEventListener("click", () => {
-       const userChoice = myChoice.getAttribute("id");
-        const compChoice = getCompChoice();
-    console.log("choice was clicked");
+  myChoice.addEventListener("click", ) => {
+ const userChoice = myChoice.getAttribute("id")
+       const compChoice = getCompChoice()
+    })
+})
 
 
-if (userChoice == compChoice) {
-    console.log("game was draw!");
-}
-    });
-});
+
+
+   
+ 
+// if (userChoice === compChoice) {
+//     console.log("game was draw!");
+// }
+
+
