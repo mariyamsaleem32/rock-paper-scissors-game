ccc16bb4c9a8c95da43cb03d0d841421f16e3e0e
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-10-13 13:15:55 -0700

    update scrip file

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-// console.log("one");
-// console.log("two");
-
-// setTimeout( () => {
-
-//     console.log("three");
-
-// }, 2000);
-
 const choice = document.querySelectorAll(".choice");
 
-const getCompChoice = ( () => {
+    const getCompChoice = ( () => {
+
     let chicesOfComp = ["rock","paper","scissors "];
     let indx = Math.floor(Math.random()*3);
      return chicesOfComp[indx]
+
     });
 
 choice.forEach( (myChoice) => {
-  myChoice.addEventListener("click", ) => {
- const userChoice = myChoice.getAttribute("id")
-       const compChoice = getCompChoice()
-    })
-})
+myChoice.addEventListener("click",()  => {
+
+  const userChoice = myChoice.getAttribute("id")
+  const compChoice = getCompChoice();
+
+    });
+});
 
 
 
