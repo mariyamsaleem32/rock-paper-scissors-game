e10c6a14d556b7881bb182c62278ccfab3916127
mariyamsaleem32 <164928689+mariyamsaleem32@users.noreply.github.com>
2024-10-13 14:42:56 -0700

    add functionality

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-const choice = document.querySelectorAll(".choice");
+const choices = document.querySelectorAll(".choice");
 
-    const getCompChoice = ( () => {
-
-    let chicesOfComp = ["rock","paper","scissors "];
+ const getCompChoice = ( () => {
+    let choiceOfComp = ["rock","paper","scissor"];
     let indx = Math.floor(Math.random()*3);
-     return chicesOfComp[indx]
-
+     return choiceOfComp[indx];
     });
 
-choice.forEach( (myChoice) => {
-myChoice.addEventListener("click",()  => {
+    choices.forEach((choice) =>{
+        choice.addEventListener("click", () =>{
+        
+            let userChoice = choice.id;
+            console.log("your choice" ,userChoice);
+            let compChoice = getCompChoice();
+            console.log("comp choice",compChoice);
 
-  const userChoice = myChoice.getAttribute("id")
-  const compChoice = getCompChoice();
+if (userChoice === compChoice) {
+    console.log("game was draw!")
 
-    });
+}if (userChoice === "scissor" && compChoice === "paper") {
+    console.log(`your ${userChoice} beats comuter's ${compChoice} `);
+
+ } if(userChoice === "paper" && compChoice === "scissor") {
+    console.log(`computer's ${compChoice} beats your ${userChoice} `);
+
+} if (userChoice === "paper" && compChoice === "rock") {
+    console.log(`your ${userChoice} beats comuter's ${compChoice} `);
+    
+} if (userChoice === "rock" && compChoice === "paper") {
+    console.log(`computer's ${compChoice} beats your ${userChoice} `);
+    
+} if(userChoice === "rock" && compChoice === "scissor") {
+    console.log(`your ${userChoice} beats comuter's ${compChoice} `);
+    
+}if (userChoice === "scissor" && compChoice === "rock") {
+    console.log(`computer's ${compChoice} beats your ${userChoice} `);
+ }
+
 });
-
-
-
-
-   
- 
-// if (userChoice === compChoice) {
-//     console.log("game was draw!");
-// }
-
-
+});
